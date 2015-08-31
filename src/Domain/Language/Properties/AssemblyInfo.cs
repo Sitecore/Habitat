0fa875f75db4204abf3ca6676c0696caabfe24db
Thomas Eldblom <the@sitecore.net>
2015-08-31 21:27:06 +1000

    Fixed Languages project Added missing item serialization

--- src/Domain/Language/Properties/AssemblyInfo.cs
+++ src/Domain/Language/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Language")]
+[assembly: AssemblyTitle("Habitat.Language")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Language")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyProduct("Habitat")]
+[assembly: AssemblyCopyright("Copyright © Sitecore 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
---
+++ src/Domain/Language/README.md
@@ -0,0 +1,5 @@
+#Habitat.Language
+========
+
+##What is the goal of this project?
+--------------------------------
