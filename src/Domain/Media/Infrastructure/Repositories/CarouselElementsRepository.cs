876038d3146dd241fe019a913664b4708e42c865
Alin Marius Pârjolea <amp@sitecore.net>
2015-09-02 11:05:13 +0200

    cleanup

--- src/Domain/Media/Infrastructure/Repositories/CarouselElementsRepository.cs
+++ src/Domain/Media/Infrastructure/Repositories/CarouselElementsRepository.cs
@@ -5,7 +5,7 @@ using Sitecore.Data.Items;
 
 namespace Habitat.Media.Infrastructure.Repositories
 {
-    public class CarouselElementsRepository
+    public static class CarouselElementsRepository
     {
         public static IEnumerable<CarouselElement> Get(Item item, string linkClass = "", string imageClass = "",
             string textClass = "")
@@ -13,24 +13,13 @@ namespace Habitat.Media.Infrastructure.Repositories
             var active = "active";
             foreach (var child in item.GetMultiListValues(Templates.HasMediaSelector.Fields.MediaSelector))
             {
-                if (child.IsDerived(Templates.HasMediaImage.ID))
+                if (!child.IsDerived(Templates.HasMediaImage.ID)) continue;
+                yield return new CarouselElement
                 {
-                    //ImageSpot imageSpot = ImageSpotRepository.Get(child, linkClass, imageClass, textClass);
-                    //yield return new CarouselElement
-                    //{
-                    //    Active = active,
-                    //    Image = imageSpot.Image,
-                    //    Text = imageSpot.Text,
-                    //    Link = imageSpot.Link,
-                    //    Title = imageSpot.Title
-                    //};
-                    yield return new CarouselElement
-                    {
-                        Item = child,
-                        Active = active
-                    };
-                    active = "false";
-                }
+                    Item = child,
+                    Active = active
+                };
+                active = "false";
             }
         }
     }
