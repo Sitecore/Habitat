8b99a012a42dc57a8492d8dd262615a24a8bde90
Alin Marius Pârjolea <amp@sitecore.net>
2015-09-09 14:03:39 +0200

    added search framework

---
+++ src/Framework/Search/Infrastructure/CustomSearchResult.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using Sitecore.ContentSearch;
+using Sitecore.ContentSearch.Converters;
+using Sitecore.ContentSearch.SearchTypes;
+using Sitecore.Data;
+using Sitecore.Links;
+
+namespace Habitat.Framework.Search.Infrastructure
+{
+    public class CustomSearchResult : SearchResultItem
+    {
+        public string Title { get; set; }
+        public string Summary { get; set; }
+        public string Body { get; set; }
+
+        public override string Url
+        {
+            get { return LinkManager.GetItemUrl(GetItem()); }
+        }
+
+        [TypeConverter(typeof(IndexFieldEnumerableConverter))]
+        [IndexField("__base_template")]
+        public IEnumerable<ID> BaseTemplate { get; set; }
+    }
+}
---
+++ src/Framework/Search/Infrastructure/SearchResultTotalAggregate.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Habitat.Framework.Search.Infrastructure
+{
+    internal class SearchResultTotalAggregate
+    {
+        public string QueryWord { get; set; }
+        public string TotalCountOfResults { get; set; }
+        public IEnumerable<CustomSearchResult> SearchResults { get; set; }
+    }
+}
