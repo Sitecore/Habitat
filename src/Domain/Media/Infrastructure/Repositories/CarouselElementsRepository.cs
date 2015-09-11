1887e4c3e5b5c48e1a4b5c9cb18b9e6eccb9a768
Thomas Eldblom <the@sitecore.net>
2015-09-11 12:21:33 +1000

    Added media

--- src/Domain/Media/Infrastructure/Repositories/CarouselElementsRepository.cs
+++ src/Domain/Media/Infrastructure/Repositories/CarouselElementsRepository.cs
@@ -11,7 +11,7 @@ namespace Habitat.Media.Infrastructure.Repositories
         public static IEnumerable<CarouselElement> Get(Item item)
         {
             var active = "active";
-            foreach (var child in item.GetMultiListValues(Templates.HasMediaSelector.Fields.MediaSelector).Where(i => i.IsDerived(Templates.HasMediaImage.ID)))
+            foreach (var child in item.GetMultiListValues(Templates.HasMediaSelector.Fields.MediaSelector).Where(i => i.IsDerived(Templates.HasMedia.ID)))
             {
                 yield return new CarouselElement
                 {
