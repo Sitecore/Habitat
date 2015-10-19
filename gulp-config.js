d3dafc62f76ad6f99dd5374195f5678112ca0cf5
Thomas Eldblom <the@sitecore.net>
2015-10-19 20:31:44 +1100

    *** MASSIVE UPDATE: Please reset your local website before updating *****
    Upgraded to Sitecore 8.1
    Upgraded to Unicorn 3
    Moved local domain to http://habitat.local

--- gulp-config.js
+++ gulp-config.js
@@ -1,7 +1,7 @@
 module.exports = function() {
     var config = {
-        websiteRoot :"C:\\Websites\\habitat\\Website", 
-        sitecoreLibraries: "C:\\Websites\\habitat\\Website\\bin",
+        websiteRoot: "C:\\websites\\Habitat.local\\Website",
+        sitecoreLibraries: "C:\\websites\\Habitat.local\\Website\\bin",
         solutionName: "Habitat"
     }
     return config;
