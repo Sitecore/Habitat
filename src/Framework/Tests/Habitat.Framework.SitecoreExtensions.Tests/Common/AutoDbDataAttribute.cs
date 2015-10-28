ef25bb1f023a5bef140354c00b835d21109dd333
dmd <dmd@sitecore.net>
2015-10-28 11:52:07 +0200

    added sample test

---
+++ src/Framework/Tests/Habitat.Framework.SitecoreExtensions.Tests/Common/AutoDbDataAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.Xunit2;
+using Sitecore.FakeDb.AutoFixture;
+
+namespace Habitat.Framework.SitecoreExtensions.Tests.Common
+{
+  internal class AutoDbDataAttribute : AutoDataAttribute
+  {
+    public AutoDbDataAttribute()
+      : base(new Fixture().Customize(new AutoDbCustomization()))
+    {
+    }
+  }
+}
