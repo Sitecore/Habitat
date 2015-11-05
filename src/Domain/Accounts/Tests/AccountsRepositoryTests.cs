ff3fa723e45f46586f0c85c048fee33182d5e260
dmd <dmd@sitecore.net>
2015-11-05 15:03:15 +0200

    added reset password method to repository

---
+++ src/Domain/Accounts/Tests/AccountsRepositoryTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Web.Mvc;
+using System.Web.Security;
+using FluentAssertions;
+using Habitat.Accounts.Controllers;
+using Habitat.Accounts.Repositories;
+using Moq;
+using Sitecore;
+using Sitecore.Collections;
+using Sitecore.Data;
+using Sitecore.FakeDb;
+using Sitecore.FakeDb.AutoFixture;
+using Sitecore.FakeDb.Security.Accounts;
+using Sitecore.FakeDb.Sites;
+using Sitecore.Globalization;
+using Sitecore.Sites;
+using Xunit;
+
+namespace Habitat.Accounts.Tests
+{
+  public class AccountsRepositoryTests
+  {
+    [Theory]
+    [AutoDbData]
+    public void RestorePasswordShouldReturnsNewPassword(Mock<FakeMembershipUser> user, Mock<MembershipProvider> membershipProvider, AccountRepository repo)
+    {
+      user.Setup(x => x.ProviderName).Returns("fake");
+      membershipProvider.Setup(x=>x.ResetPassword(It.IsAny<string>(), It.IsAny<string>())).Returns("new password");
+      membershipProvider.Setup(x=>x.Name).Returns("name");
+      membershipProvider.Setup(x=>x.GetUser(It.IsAny<string>(), It.IsAny<bool>())).Returns(user.Object);
+
+      using (new Sitecore.FakeDb.Security.Web.MembershipSwitcher(membershipProvider.Object))
+      {
+        repo.RestorePassword(@"extranet\John").Should().Be("new password");
+      }
+    }
+  
+  }
+}
