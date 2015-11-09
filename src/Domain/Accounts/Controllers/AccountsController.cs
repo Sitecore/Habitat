80b51ee23f3824880216464daa6acf9d4aa52441
OlegJytnik <olj@sitecore.net>
2015-11-09 19:57:49 +0200

    Service page layout restructured.

--- src/Domain/Accounts/Controllers/AccountsController.cs
+++ src/Domain/Accounts/Controllers/AccountsController.cs
@@ -68,9 +68,12 @@ namespace Habitat.Accounts.Controllers
     [HttpGet]
     public ActionResult Login()
     {
-      if (Context.User.IsAuthenticated)
+      if (Context.PageMode.IsNormal)
       {
-        this.Response.Redirect("/");
+        if (Context.User.IsAuthenticated)
+        {
+          this.Response.Redirect("/");
+        }
       }
 
       return this.View();
