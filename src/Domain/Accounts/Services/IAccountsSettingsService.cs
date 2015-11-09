20eccde5ffa9716fb297f814797f6a9f32220ead
dmd <dmd@sitecore.net>
2015-11-09 16:13:38 +0200

    Added template for password recovery mail

---
+++ src/Domain/Accounts/Services/IAccountsSettingsService.cs
@@ -0,0 +1,12 @@
+﻿using System.Net.Mail;
+using Sitecore.Data;
+using Sitecore.Data.Items;
+
+namespace Habitat.Accounts.Services
+{
+  public interface IAccountsSettingsService
+  {
+    string GetPageLink(Item contextItem, ID fieldID);
+    MailMessage GetForgotPasswordMailTemplate();
+  }
+}
