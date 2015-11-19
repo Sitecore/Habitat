b73eac8d7ce9600503eed90b9ba81316dffa67c7
goran.halvarsson <goranhalvarsson@gmail.com>
2015-11-19 22:56:53 +0100

    Added ProjectOxfordAI test project

---
+++ src/Framework/ProjectOxfordAI/Tests/AutoDbDataAttribute.cs
@@ -0,0 +1,14 @@
+﻿namespace Habitat.Framework.ProjectOxfordAI.Tests { 
+
+  using Ploeh.AutoFixture;
+  using Ploeh.AutoFixture.Xunit2;
+  using Sitecore.FakeDb.AutoFixture;
+
+  internal class AutoDbDataAttribute : AutoDataAttribute
+  {
+    public AutoDbDataAttribute()
+      : base(new Fixture().Customize(new AutoDbCustomization()))
+    {
+    }
+  }
+}
