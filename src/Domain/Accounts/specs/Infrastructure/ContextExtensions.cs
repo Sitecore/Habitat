a561d8bfe8184c0f4309171fdf5f996c13feefc1
dmd <dmd@sitecore.net>
2015-11-20 15:44:36 +0200

    Added structure to support cleanup

---
+++ src/Domain/Accounts/specs/Infrastructure/ContextExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Habitat.Accounts.Specflow.Steps
+{
+  using TechTalk.SpecFlow;
+
+  static class ContextExtensions
+  {
+    public static List<TestCleanupAction> CleanupPool {
+      get
+      {
+        if (!ScenarioContext.Current.ContainsKey("cleanup"))
+        {
+          ScenarioContext.Current.Add("cleanup", new List<TestCleanupAction>());
+        }
+        
+        return ScenarioContext.Current.Get<List<TestCleanupAction>>("cleanup");
+      }
+    }
+      
+  }
+}
