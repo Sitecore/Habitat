d0c779676a7bee03953edd7d2abd3bbb1ced4935
OlegJytnik <olj@sitecore.net>
2015-11-26 18:13:06 +0200

    Unit test for Search controller.

--- src/Domain/Search/Tests/SearchControllerTests.cs
+++ src/Domain/Search/Tests/SearchControllerTests.cs
@@ -29,5 +29,26 @@
       var result = controller.PagedSearchResults(query, null) as ViewResult;
       result.Model.Should().BeOfType<PagedSearchResults>();
     }
+
+    [Theory]
+    [AutoDbData]
+    public void ShouldReturnSearchSettings(ISearchResults searchResults,[Substitute] SearchService service, SearchSettings searchSettings, ISearchServiceRepository serviceRepository, ISearchSettingsRepository settingsRepository, QueryRepository queryRepository,   IRenderingPropertiesRepository renderingPropertiesRepository, string query)
+    {
+      settingsRepository.Get(Arg.Any<string>()).Returns(searchSettings);
+      var controller = new SearchController(serviceRepository, settingsRepository, queryRepository, renderingPropertiesRepository);
+      var result = controller.SearchSettings(query) as ViewResult;
+      result.Model.Should().BeOfType<SearchSettings>();
+    }
+
+    [Theory]
+    [AutoDbData]
+    public void ShouldReturnSearchResults(ISearchResults searchResults, [Substitute] SearchService service, ISearchServiceRepository serviceRepository, ISearchSettingsRepository settingsRepository, QueryRepository queryRepository, IRenderingPropertiesRepository renderingPropertiesRepository, string query)
+    {
+      service.Search(Arg.Any<IQuery>()).Returns(searchResults);
+      serviceRepository.Get().Returns(service);
+      var controller = new SearchController(serviceRepository, settingsRepository, queryRepository, renderingPropertiesRepository);
+      var result = controller.SearchResults(query) as ViewResult;
+      result.Model.Should().As<ISearchResults>();
+    }
   }
 }
