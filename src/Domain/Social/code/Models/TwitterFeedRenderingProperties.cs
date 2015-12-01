723649256dacce752703783cf056b2b6e0c25f1a
OlegJytnik <olj@sitecore.net>
2015-12-01 15:00:42 +0200

    Number of twitts now configurable through the rendering properties.

---
+++ src/Domain/Social/code/Models/TwitterFeedRenderingProperties.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Habitat.Social.Models
+{
+  public class TwitterFeedRenderingProperties
+  {
+    private int twittsToShow;
+    private const int DefaultTwittsToShow = 2;
+    public int TwittsToShow
+    {
+      get
+      {
+        if (twittsToShow == 0)
+        {
+          this.twittsToShow = DefaultTwittsToShow;
+        }
+
+        return this.twittsToShow;
+      }
+      set
+      {
+        this.twittsToShow = value;
+      }
+    }
+  }
+}
