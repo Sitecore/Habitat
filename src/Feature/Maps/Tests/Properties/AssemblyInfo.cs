6439e72fcee2c2001efd405e583f380b0945eca7
Rey Rahadian <rahadian.rey@gmail.com>
2015-12-07 17:43:36 +0700

    update the asssemblyinfo names

--- src/Feature/Maps/Tests/Properties/AssemblyInfo.cs
+++ src/Feature/Maps/Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Habitat.Maps.Tests")]
+[assembly: AssemblyTitle("Sitecore.Feature.Maps.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Habitat.Maps.Tests")]
+[assembly: AssemblyProduct("Sitecore.Feature.Maps.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- src/Feature/Maps/specs/Properties/AssemblyInfo.cs
+++ src/Feature/Maps/specs/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Habitat.Maps.Specflow")]
+[assembly: AssemblyTitle("Sitecore.Feature.Maps.Specflow")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Habitat.Maps.Specflow")]
+[assembly: AssemblyProduct("Sitecore.Feature.Maps.Specflow")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
