5ccfc8c6f5efc99a598e502bb13d16120d8582c9
OlegJytnik <olj@sitecore.net>
2015-12-09 17:51:50 +0200

    Initial commit site switcher feature

---
+++ src/Feature/Multisite/code/Controllers/MultisiteController.cs
@@ -0,0 +1,12 @@
+﻿namespace Sitecore.Feature.Multisite.Controllers
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Web;
+  using System.Web.Mvc;
+
+  public class MultisiteController : Controller
+  {
+  }
+}
---
+++ src/Feature/Multisite/code/Templates.cs
@@ -0,0 +1,22 @@
+﻿namespace Sitecore.Feature.Multisite
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Web;
+  using Sitecore.Data;
+
+  public class Templates
+  {
+    public struct SiteConfiguration
+    {
+      public static ID ID = new ID("{0FCCFE4F-B087-498F-BD26-5CDFFC522C9A}");
+
+      public struct Fields
+      {
+        public static readonly ID HostName = new ID("{12537182-F35C-403F-AFB5-747D55C450B8}");
+      }
+    }
+  }
+
+}
