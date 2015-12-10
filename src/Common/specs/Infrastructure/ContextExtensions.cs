125daf2ffb2785d3f3c456c0a129082911fa0d61
dmd <dmd@sitecore.net>
2015-12-10 13:44:43 +0200

    Added service references

---
+++ src/Common/specs/Infrastructure/ContextExtensions.cs
@@ -0,0 +1,26 @@
+﻿namespace Common.Specflow.Infrastructure
+{
+  using System.Collections.Generic;
+  using System.ServiceModel;
+  using Common.Specflow.TestHelperService;
+  using Habitat.Accounts.Specflow.Steps;
+  using TechTalk.SpecFlow;
+
+  internal static class ContextExtensions
+  {
+    public static List<TestCleanupAction> CleanupPool
+    {
+      get
+      {
+        if (!ScenarioContext.Current.ContainsKey("cleanup"))
+        {
+          ScenarioContext.Current.Add("cleanup", new List<TestCleanupAction>());
+        }
+
+        return ScenarioContext.Current.Get<List<TestCleanupAction>>("cleanup");
+      }
+    }
+
+    public static AutoTestsHelperServiceSoapClient HelperService => new AutoTestsHelperServiceSoapClient(new BasicHttpBinding(), new EndpointAddress(new Settings().TestHelperService));
+  }
+}
---
+++ src/Common/specs/Infrastructure/Settings.cs
@@ -0,0 +1,21 @@
+﻿namespace Habitat.Accounts.Specflow.Steps
+{
+  using System.Configuration;
+
+  public class Settings
+  {
+    public string BaseUrl => ConfigurationManager.AppSettings["baseUrl"];
+    public string RegisterPageUrl => ConfigurationManager.AppSettings["registerUrl"];
+
+    public string LoginPageUrl => ConfigurationManager.AppSettings["loginPageUrl"];
+
+    public string EditUserProfileUrl => ConfigurationManager.AppSettings["editUserProfileUrl"];
+
+    public string ContactUsPageUrl => ConfigurationManager.AppSettings["contactUsPageUrl"];
+
+    public string TestHelperService => BaseUrl + ConfigurationManager.AppSettings["testsProxyUrl"];
+
+    public string ForgotPasswordPageUrl => ConfigurationManager.AppSettings["forgotPasswordUrl"];
+    public string EndSessionUrl => ConfigurationManager.AppSettings["endSessionUrl"];
+  }
+}
