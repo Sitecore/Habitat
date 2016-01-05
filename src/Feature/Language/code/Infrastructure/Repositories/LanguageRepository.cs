972942eb44a7a11019ce33bcf6095d9bcb2892aa
OlegJytnik <olj@sitecore.net>
2016-01-05 17:23:54 +0200

    Language selector has been updated in order to support language settings.

--- src/Feature/Language/code/Infrastructure/Repositories/LanguageRepository.cs
+++ src/Feature/Language/code/Infrastructure/Repositories/LanguageRepository.cs
@@ -3,8 +3,11 @@
   using System.Collections.Generic;
   using System.Linq;
   using Sitecore;
+  using Sitecore.Data.Fields;
   using Sitecore.Feature.Language.Infrastructure.Factories;
   using Sitecore.Feature.Language.Models;
+  using Sitecore.Foundation.MultiSite;
+  using Sitecore.Foundation.SitecoreExtensions.Extensions;
 
   public static class LanguageRepository
   {
@@ -18,5 +21,32 @@
     {
       return LanguageFactory.Create(Context.Language);
     }
+
+    public static IEnumerable<Language> GetSupportedLanguages()
+    {
+      var languages = GetAll();
+      var siteContext = new SiteContext();
+      var siteDefinition = siteContext.GetSiteDefinition(Sitecore.Context.Item);
+      
+      if (siteDefinition?.Item == null)
+      {
+        return languages;
+      }
+
+      if (siteDefinition.Item.IsDerived(Feature.Language.Templates.LanguageSettings.ID))
+      {
+        var supportedLanguagesField = new MultilistField(siteDefinition.Item.Fields[Feature.Language.Templates.LanguageSettings.Fields.SupportedLanguages]);
+        if (supportedLanguagesField.Count == 0)
+        {
+          return languages;
+        }
+
+        var supportedLanguages = supportedLanguagesField.GetItems();
+
+        languages = languages.Where(language => supportedLanguages.Any(item => item.Name.Equals(language.TwoLetterCode)));
+      }
+
+      return languages;
+    } 
   }
 }
