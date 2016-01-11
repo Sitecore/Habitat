5192a49010f8e0c393681ab1df4e9c8cab6691ad
OlegJytnik <olj@sitecore.net>
2016-01-11 18:16:55 +0200

    Unit tests for MetaKeywordsModel

--- src/Feature/Metadata/Tests/MetaKeywordsModelTests.cs
+++ src/Feature/Metadata/Tests/MetaKeywordsModelTests.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace Sitecore.Feature.Metadata.Tests
 {
+  using FluentAssertions;
+  using Sitecore.Feature.Metadata.Models;
+  using Sitecore.Feature.Metadata.Tests.Extensions;
+  using Xunit;
+
   public class MetaKeywordsModelTests
   {
+    [Theory]
+    [AutoDbData]
+    public void ToStrign_ShouldReturnCommaSeparatedListOfKeywords(List<string> keywords, MetaKeywordsModel model)
+    {
+      model.Keywords = keywords;
+      var result = model.ToString();
+      var keywordCollection = result.Split(new [] {','}, StringSplitOptions.RemoveEmptyEntries).ToList();
+      var intersection = keywordCollection.Except(keywords);
+      intersection.Count().Should().Be(0);
+    }
   }
 }
