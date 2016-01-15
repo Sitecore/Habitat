4a9f8eebfb9319601382c8853cda1032c6d979e6
dmd <dmd@sitecore.net>
2016-01-15 17:25:04 +0200

    Fixed removed tests

---
+++ src/Feature/Media/Tests/Infrastructure/MediaSelectorTemplate.cs
@@ -0,0 +1,21 @@
+namespace Sitecore.Feature.Media.Tests.Infrastructure
+{
+  using Sitecore.FakeDb;
+
+  public class MediaSelectorTemplate : DbTemplate
+  {
+    public MediaSelectorTemplate() : base(Templates.HasMediaSelector.ID)
+    {
+      base.Add(Templates.HasMediaSelector.Fields.MediaSelector);
+    }
+  }
+}
+
+namespace Sitecore.Feature.Media.Tests.Infrastructure
+{
+}
+
+namespace Sitecore.Feature.Media.Tests.Infrastructure
+{
+}
+
