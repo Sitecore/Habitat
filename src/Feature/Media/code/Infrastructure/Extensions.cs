948afdb3a9b96e6e8b84a977d481d5e6bf84a9b3
dmd <dmd@sitecore.net>
2016-01-15 14:52:46 +0200

    added demo page for parallax and fixed script behaviour to use default css behavior for pictures

--- src/Feature/Media/code/Infrastructure/Extensions.cs
+++ src/Feature/Media/code/Infrastructure/Extensions.cs
@@ -24,7 +24,10 @@ namespace Sitecore.Feature.Media.Infrastructure
       var mediaFormat = mimeType[1];
 
       attributeDictionary["type"] = mediaType;
-      attributeDictionary["format"] = mediaFormat;
+      if (mediaType == "video")
+        attributeDictionary["format"] = mediaFormat;
+        
+      attributeDictionary[mediaType == "video" ? $"url-{mediaFormat}" : "url"] = item.MediaUrl(Templates.HasParallaxBackground.Fields.BackgroundMedia);
       var checkboxField = (Sitecore.Data.Fields.CheckboxField)item.Fields[Templates.HasParallaxBackground.Fields.IsParallaxEnabled];
       attributeDictionary["attachment"] = checkboxField.Checked ? "parallax" : "static";
       if (checkboxField.Checked)
@@ -32,7 +35,7 @@ namespace Sitecore.Feature.Media.Infrastructure
         attributeDictionary["parallaxspeed"] = item.Fields[Templates.HasParallaxBackground.Fields.ParallaxSpeed].Value;
       }
 
-      attributeDictionary[mediaType == "video" ? $"url-{mediaFormat}" : "url"] = item.MediaUrl(Templates.HasParallaxBackground.Fields.BackgroundMedia);
+      
       var attributes = attributeDictionary.Select(x => $"data-multibackground-layer-0-{x.Key}='{x.Value}'").ToList();
       attributes.Add("data-multibackground");
       return new HtmlString(string.Join(" ", attributes));
