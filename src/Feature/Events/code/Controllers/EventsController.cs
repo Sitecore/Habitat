6cb2136f014b35bba0484438f5d3b0082fae552b
Azadeh Khojandi <azadehkhojandi@gmail.com>
2016-01-30 18:42:10 +1100

    move events in json to another controller

--- src/Feature/Events/code/Controllers/EventsController.cs
+++ src/Feature/Events/code/Controllers/EventsController.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 using System.Web.Mvc;
 
 namespace Sitecore.Feature.Events.Controllers
 {
+    using System.Globalization;
     using Sitecore.Feature.Events.Repositories;
     using Sitecore.Mvc.Presentation;
 
@@ -39,17 +37,7 @@ namespace Sitecore.Feature.Events.Controllers
             return this.View();
         }
 
-        [HttpGet]
-        public ActionResult GetEventsListJson()
-        {
-            var events = new List<object>
-            {
-                new {title = "EventsControllerevent1",startsAtTxt=System.DateTime.UtcNow.ToString(CultureInfo.InvariantCulture)},
-                new {title = "EventsControllerevent2",startsAtTxt=System.DateTime.UtcNow.AddDays(2).ToString(CultureInfo.InvariantCulture)},
-                new {title = "EventsControllerevent3",startsAtTxt=System.DateTime.UtcNow.AddDays(4).ToString(CultureInfo.InvariantCulture)}
-            };
-            return Json(events.ToArray(), JsonRequestBehavior.AllowGet);
-        }
+        
 
     }
 }
