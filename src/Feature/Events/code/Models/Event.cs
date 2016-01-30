bf8c972cf3b6085cbdba593e86ef305302779b5d
Sudeep Shakya <sudeep.shakya@neonstingray.com>
2016-01-30 18:51:43 +1100

    Event model in progress

---
+++ src/Feature/Events/code/Models/Event.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Feature.Events.Models
+{
+    using Sitecore.Data.Fields;
+    using Sitecore.Data.Items;
+
+    public class EventModel
+    {
+        public string Title { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime StartTime { get; set; }
+        public Item Item { get; set; }
+
+        public EventModel()
+        {
+            
+        }
+
+        /// <summary>
+        /// Create event model from the event item
+        /// </summary>
+        /// <param name="item"></param>
+        public EventModel(Item item)
+        {
+            Item = item;
+
+            //fields
+            Title = item[Templates.Event.Fields.Title];            
+        }
+    }
+}
