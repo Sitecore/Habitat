55e03b4f0c7d9f997aab6cb7edacf08a52f56dd4
Sudeep Shakya <sudeep.shakya@neonstingray.com>
2016-01-30 18:15:39 +1100

    events templates, renderings and code update

--- src/Feature/Events/code/Controllers/EventsController.cs
+++ src/Feature/Events/code/Controllers/EventsController.cs
@@ -6,19 +6,35 @@ using System.Web.Mvc;
 
 namespace Sitecore.Feature.Events.Controllers
 {
+    using Sitecore.Feature.Events.Repositories;
+    using Sitecore.Mvc.Presentation;
+
     public class EventsController : Controller
     {
-        public ActionResult List()
+
+        private readonly IEventRepository _eventRepository;
+
+        public EventsController() : this(new EventRepository(RenderingContext.Current.Rendering.Item))
+        {
+        }
+
+        public EventsController(IEventRepository newsRepository)
+        {
+            this._eventRepository = newsRepository;
+        }
+
+        public ActionResult EventList()
+        {
+            var items = this._eventRepository.Get();
+            return this.View(items);
+        }
+
+        public ActionResult EventCalendar()
         {
             return this.View();
         }
 
-        public ActionResult Calendar()
-        {
-            return this.View();
-        }
-
-        public ActionResult Detail()
+        public ActionResult EventDetail()
         {
             return this.View();
         }
