8a346ac97f69f62adebddb05d34211812ce7e1f6
OlegJytnik <olj@sitecore.net>
2016-02-05 16:02:42 +0200

    MediaBackground section has been implemented.

---
+++ src/Feature/Media/code/Controllers/MediaController.cs
@@ -0,0 +1,21 @@
+﻿namespace Sitecore.Feature.Media.Controllers
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Web;
+  using System.Web.Mvc;
+  using Sitecore.Feature.Media.Infrastructure.Models;
+  using Sitecore.Foundation.SitecoreExtensions.Repositories;
+
+  public class MediaController : Controller
+  {
+    public ActionResult MediaBackgroundSection()
+    {
+      var renderingPropertiesRepository = new RenderingPropertiesRepository();
+      var mediamBackgroundModel = renderingPropertiesRepository.Get<MediaBackgroundRenderingModel>();
+
+      return this.View(mediamBackgroundModel);
+    }
+  }
+}
---
+++ src/Feature/Media/code/Infrastructure/Models/MediaBackgroundRenderingModel.cs
@@ -0,0 +1,61 @@
+﻿namespace Sitecore.Feature.Media.Infrastructure.Models
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Text;
+  using System.Web;
+  using System.Xml.Linq;
+  using Sitecore.Data.Fields;
+  using Sitecore.Data.Items;
+  using Sitecore.Resources.Media;
+  using Sitecore.Web;
+
+  public class MediaBackgroundRenderingModel
+  {
+    public string Type { get; set; }
+
+    public string Media { get; set; }
+
+    public string Parallax { get; set; }
+
+    public string MediaType { get; set; }
+
+    public string MediaFormat { get; set; }
+
+    public string CssClass
+    {
+      get
+      {
+        var classes = new List<string>();
+        classes.Add(this.Type);
+        if (this.Parallax != null && this.Parallax.Equals("1"))
+        {
+          classes.Add("bg-parallax");
+        }
+
+        return string.Join(" ", classes);
+      }
+    }
+
+    public string MediaAttribute
+    {
+      get
+      {
+        var mediaUrl = string.Empty;
+        var linkValue = HttpUtility.UrlDecode(this.Media);
+        var linkElement = XElement.Parse(linkValue);
+        var id = linkElement.Attribute("id")?.Value;
+        var item = Context.Database.GetItem(id);
+
+        if (item != null)
+        {
+          var mediaItem = new MediaItem(item);
+          mediaUrl = MediaManager.GetMediaUrl(mediaItem);
+        }
+
+        return $"style=background-image:url('{mediaUrl}');";
+      }
+    }
+  }
+}
