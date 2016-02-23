6f08b27820954297864b97f6d71dea063a4d79e1
Thomas Eldblom <the@sitecore.net>
2016-02-23 15:23:41 +1100

    Fixed problems with missing files Fixed problem with WFFM loading bootstrap.js twice

--- src/Foundation/Theming/code/bower_components/modernizr/feature-detects/json.js
+++ src/Foundation/Theming/code/bower_components/modernizr/feature-detects/json.js
@@ -1,21 +1,7 @@
-/*!
-{
-  "name": "JSON",
-  "property": "json",
-  "caniuse": "json",
-  "notes": [{
-    "name": "MDN documentation",
-    "href": "http://developer.mozilla.org/en/JSON"
-  }],
-  "polyfills": ["json2"]
-}
-!*/
-/* DOC
-Detects native support for JSON handling functions.
-*/
-define(['Modernizr'], function(Modernizr) {
-  // this will also succeed if you've loaded the JSON2.js polyfill ahead of time
-  //   ... but that should be obvious. :)
+// native JSON support.
+// developer.mozilla.org/en/JSON
 
-  Modernizr.addTest('json', 'JSON' in window && 'parse' in JSON && 'stringify' in JSON);
-});
+// this will also succeed if you've loaded the JSON2.js polyfill ahead of time
+//   ... but that should be obvious. :)
+
+Modernizr.addTest('json', !!window.JSON && !!JSON.parse);
