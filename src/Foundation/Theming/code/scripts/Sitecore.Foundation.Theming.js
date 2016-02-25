e9202a094bdf6db3e1293dd77e854a21e2d983a5
Thomas Eldblom <the@sitecore.net>
2016-02-25 15:27:19 +1100

    Split Theming main.js into multiple files

--- src/Foundation/Theming/code/scripts/Sitecore.Foundation.Theming.js
+++ src/Foundation/Theming/code/scripts/Sitecore.Foundation.Theming.js
@@ -17752,44 +17752,27 @@ if (typeof Object.create !== "function") {
         afterLazyLoad: false
     };
 }(jQuery, window, document));
-if(!window.console){
-  console={};
-  console.log = function(){};
-}
+jQuery.noConflict();
+
+(function($) {
+  $(function() {
+
+    $('.owl-carousel').each(function(){
+      var options = (typeof $(this).attr('data-options') !== "undefined") ? $.parseJSON($(this).attr('data-options')) : {};
+      options.navigationText = [
+        '<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>',
+        '<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>'
+      ];
+      $(this).owlCarousel(options);
+    });
+    
+  });
+})(jQuery);
 
 jQuery.noConflict();
 
-(function ($) {
+(function($) {
   $(function() {
-    var mq;
-
-    var $mqElement = $($.parseHTML('<span id="mq-detector"><span class="visible-xs"></span><span class="visible-sm"></span><span class="visible-md"></span><span class="visible-lg"></span></span>'));
-    $mqElement.css('visibility', 'hidden');
-
-    $('body').append($mqElement);
-    $mqElement.children().each(function() {
-      if($(this).is(':visible')) {
-        mq = $(this).attr('class').substring($(this).attr('class').length-2);
-      };
-    });
-
-    $('[href="styles/white-wonder.css"]').attr('data-themetype', 'main');
-    $('[href="styles/niteflight.css"]').attr('data-themetype', 'main');
-
-    $('.sidebar.sidebar-dynamic ul li a').on('click', function(e) {
-      e.preventDefault();
-      $(this).parent().toggleClass('open');
-    });
-
-    $('body').on('click', function(e) {
-      //$(e.target).toggleClass('bg-white');
-    });
-
-    $('input[type="file"]').on('change', function(e) {
-      var fileName = $(this).val().split('\\')[$(this).val().split('\\').length-1];
-      $('label[for="'+$(this).attr('id')+'"]').html(fileName).addClass('active');
-    });
-
     $('.counter').each(function() {
       var valTo = $(this).data('to');
       var increment = valTo / ($(this).data('speed')/100);
@@ -17803,21 +17786,25 @@ jQuery.noConflict();
         }
       }, 100);
     });
+  });
+})(jQuery);
 
-    $('[data-toggle="theme"]').on('click', function() {
-      $('[data-themetype="'+$(this).attr('data-themetype')+'"]').attr('disabled', 'disabled');
-      $('[href="styles/'+$(this).attr('data-theme')+'.css"]').removeAttr('disabled');
+jQuery.noConflict();
+
+(function($) {
+  $(function() {
+    $('input[type="file"]').on('change', function(e) {
+      var fileName = $(this).val().split('\\')[$(this).val().split('\\').length-1];
+      $('label[for="'+$(this).attr('id')+'"]').html(fileName).addClass('active');
     });
 
-    $('.owl-carousel').each(function(){
-      var options = (typeof $(this).attr('data-options') !== "undefined") ? $.parseJSON($(this).attr('data-options')) : {};
-      options.navigationText = [
-        '<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>',
-        '<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>'
-      ];
-      $(this).owlCarousel(options);
-    });
+  });
+})(jQuery);
 
+jQuery.noConflict();
+
+(function($) {
+  $(function() {
     $('.grid-filter').each(function() {
       var $grid = $($(this).attr('data-grid-filter'));
       var $filters = $(this).find('[data-group]');
@@ -17847,7 +17834,32 @@ jQuery.noConflict();
         });
       });
     });
+  });
+})(jQuery);
 
+jQuery.noConflict();
+
+(function($) {
+  $(function() {
+    $(document).delegate('*[data-toggle="lightbox"]', 'click', function (event) {
+      event.preventDefault();
+      $(this).ekkoLightbox();
+    });
+  });
+})(jQuery);
+
+jQuery.noConflict();
+
+(function($) {
+  $(function() {
+    $('[data-toggle="tooltip"]').tooltip();
+  });
+})(jQuery);
+
+jQuery.noConflict();
+
+(function($) {
+  $(function() {
     $('[data-toggle-class]').on('click', function(e) {
       e.preventDefault();
       switch ($(this).data('toggle-class')) {
@@ -17857,17 +17869,18 @@ jQuery.noConflict();
       }
       $($(this).attr('data-target')).toggleClass($(this).attr('data-toggle-class'));
     });
+  });
+})(jQuery);
 
-    $('[data-toggle="popover"]').popover();
-    $('[data-toggle="tooltip"]').tooltip();
-    $('.code').each(function(){
-      var encoded = document.createTextNode($.trim($(this)[0].innerHTML));
-      $(this).html(encoded);
-      var editor = ace.edit($(this).attr('id'));
-      editor.setFontSize(13);
-      editor.getSession().setMode("ace/mode/html");
-      //$(this).html(encoded);
+jQuery.noConflict();
+
+(function($) {
+  $(function() {
+    $('.sidebar.sidebar-dynamic ul li a').on('click', function(e) {
+      e.preventDefault();
+      $(this).parent().toggleClass('open');
     });
+    
     $('body').on('click', '[data-toggle="sidebar"]', function(e) {
       e.preventDefault();
       console.log(this);
@@ -17876,12 +17889,29 @@ jQuery.noConflict();
       $('.sidebar-closed, .sidebar-opened').toggle();
     });
     
-
-    $(document).delegate('*[data-toggle="lightbox"]', 'click', function (event) {
-      event.preventDefault();
-      $(this).ekkoLightbox();
-    });
-
     !$('html').hasClass('show-sidebar-left') && !$('html').hasClass('show-sidebar-left') ? $('.sidebar-closed').show() && $('.sidebar-opened').hide() : $('.sidebar-closed').hide() && $('.sidebar-opened').show();
+  });
+})(jQuery);
+
+jQuery.noConflict();
+
+(function($) {
+  $(function() {
+    $('[href="styles/white-wonder.css"]').attr('data-themetype', 'main');
+    $('[href="styles/dayfrost.css"]').attr('data-themetype', 'main');
+    $('[href="styles/niteflight.css"]').attr('data-themetype', 'main');
+    
+    $('[data-toggle="theme"]').on('click', function() {
+      $('[data-themetype="'+$(this).attr('data-themetype')+'"]').attr('disabled', 'disabled');
+      $('[href="styles/'+$(this).attr('data-theme')+'.css"]').removeAttr('disabled');
+    });
+  });
+})(jQuery);
+
+jQuery.noConflict();
+
+(function($) {
+  $(function() {
+    $('[data-toggle="popover"]').popover();
   });
 })(jQuery);
