7ec666ced32d2d73b143f7e6c80d13b42df10937
Thomas Eldblom <the@sitecore.net>
2016-03-01 10:41:59 +1100

    Created Foundation.LocalDatasource module

---
+++ src/Foundation/LocalDatasource/code/Settings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Foundation.LocalDatasource
+{
+  public static class Settings
+  {
+    private static string LocalDatasourceFolderNameSetting = "Foundation.LocalDatasource.LocalDatasourceFolderName";
+    private static string LocalDatasourceFolderNameDefault = "_Local";
+    private static string LocalDatasourceFolderTemplateSetting = "Foundation.LocalDatasource.LocalDatasourceFolderTemplate";
+
+    public static string LocalDatasourceFolderName => Sitecore.Configuration.Settings.GetSetting(LocalDatasourceFolderNameSetting, LocalDatasourceFolderNameDefault);
+    public static string LocalDatasourceFolderTemplate => Sitecore.Configuration.Settings.GetSetting(LocalDatasourceFolderTemplateSetting);
+  }
+}
---
+++ src/Foundation/LocalDatasource/code/Templates.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Foundation.LocalDatasource
+{
+  using Sitecore.Data;
+
+  public class Templates
+  {
+    public struct RenderingOptions
+    {
+      public static ID ID = new ID("{D1592226-3898-4CE2-B190-090FD5F84A4C}");
+      public struct Fields
+      {
+        // ReSharper disable once MemberHidesStaticFromOuterClass
+        public static readonly ID SupportsLocalDatasource = new ID("{1C307764-806C-42F0-B7CE-FC173AC8372B}");
+      }
+    }
+  }
+}
