2fe0b76e1eeada5238bd8c8c09347f89533da78c
dmd <dmd@sitecore.net>
2016-03-03 17:31:33 +0200

    Added tests

--- src/Feature/Maps/tests/MapsPointsRepositoryTests.cs
+++ src/Feature/Maps/tests/MapsPointsRepositoryTests.cs
@@ -14,6 +14,8 @@
   using Models;
   using NSubstitute;
   using Pipelines;
+  using FakeDb.AutoFixture;
+  using Ploeh.AutoFixture.AutoNSubstitute;
 
   public class MapsPointsRepositoryTests
   {
@@ -21,7 +23,7 @@
     [AutoDbData]
     public void GetAll_NullPassed_ShouldThrowArgumentNullException()
     {
-      var repository  = new MapPointRepository();
+      var repository = new MapPointRepository();
       Action a = () => repository.GetAll(null);
       a.ShouldThrow<ArgumentNullException>();
     }
@@ -31,14 +33,57 @@
     public void GetAll_PointItemPassed_ShouldReturnSinglePoint(Db db)
     {
       var itemid = ID.NewID;
-      db.Add(new DbItem("point",itemid, Templates.MapPoint.ID));
+      db.Add(new DbItem("point", itemid, Templates.MapPoint.ID) { { Templates.MapPoint.Fields.Name, "nameField" } });
       var repository = new MapPointRepository();
       var actual = repository.GetAll(db.GetItem(itemid));
-      actual.Single();
+      actual.Single().Name.Should().Be("nameField");
+    }
 
+
+    [Theory]
+    [AutoDbData]
+    public void GetAll_WrongItemPassed_ShouldThrowException([Content]Data.Items.Item item)
+    {
+      var repository = new MapPointRepository();
+      Action a = () => repository.GetAll(item);
+      a.ShouldThrow<ArgumentException>();
+    }
+
+
+    [Theory]
+    [AutoDbData]
+    public void GetAll_PointFolderItemPassed_ShouldCallSearchService(Db db,Foundation.Indexing.Repositories.ISearchServiceRepository searchRepo, [Substitute]Foundation.Indexing.SearchService service)
+    {
+      var itemid = ID.NewID;
+      db.Add(new DbItem("point", itemid, Templates.MapPointsFolder.ID));
+      searchRepo.Get().Returns(service);
+      var repository = new MapPointRepository(searchRepo);
+      repository.GetAll(db.GetItem(itemid));
+      service.FindAll().Received(1);
     }
 
 
 
+    [Theory]
+    [AutoDbData]
+    public void GetAll_PointFolderItemPassed_ShouldReturnsItemsFromSearchService([Content]Data.Items.Item[] items,Db db, Foundation.Indexing.Repositories.ISearchServiceRepository searchRepo, [Substitute]Foundation.Indexing.SearchService service, Foundation.Indexing.Models.ISearchResults results, Foundation.Indexing.Models.ISearchResult result)
+    {
+      var itemid = ID.NewID;
+      db.Add(new DbItem("point", itemid, Templates.MapPointsFolder.ID));
+      searchRepo.Get().Returns(service);
+      service.FindAll().Returns(results);
+      var searchResutls = items.Select(x =>
+      {
+        var sr = Substitute.For<Foundation.Indexing.Models.ISearchResult>();
+        sr.Item.Returns(x);
+        return sr;
+      });
+
+      results.Results.Returns(searchResutls);
+
+      var repository = new MapPointRepository(searchRepo);
+      var actual = repository.GetAll(db.GetItem(itemid));
+      actual.Count().Should().Be(items.Length);
+    }
   }
 }
