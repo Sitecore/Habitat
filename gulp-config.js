ffaa924a32ba8362e7ea56b14298f68bcca0ce66
Thomas Eldblom <the@sitecore.net>
2016-03-08 15:07:11 +1100

    Added unicorn sync to gulp

--- gulp-config.js
+++ gulp-config.js
@@ -4,8 +4,9 @@ module.exports = function () {
     sitecoreLibraries: "C:\\websites\\Habitat.local\\Website\\bin",
     solutionName: "Habitat",
     buildConfiguration: "Debug",
-	unicornSecret: "kl3KGocnbp3L9ncCZYRPwFwiW7jW2UhE02T3GZU6sL5rTCKZzsRyRYLtGzuh8Li",
-	runCleanBuilds: false
-    }
+    unicornSecret: "kl3KGocnbp3L9ncCZYRPwFwiW7jW2UhE02T3GZU6sL5rTCKZzsRyRYLtGzuh8Li",
+    unicornUrl: "http://habitat.local/unicorn.aspx",
+    runCleanBuilds: false
+  }
   return config;
 }
--- scripts/Unicorn/Sync.ps1
+++ scripts/Unicorn/Sync.ps1
@@ -1,9 +1,11 @@
+param([string]$url, [string]$secret, [string]$configurations)
 $ErrorActionPreference = 'Stop'
 
 # This is an example PowerShell script that will remotely execute a Unicorn sync using the new CHAP authentication system.
 
 Import-Module .\Unicorn.psm1
+$configs = $configurations.Split('^')
 
-Sync-Unicorn -ControlPanelUrl 'https://localhost/unicorn.aspx' -SharedSecret 'your-sharedsecret-here' -Configurations @('Test1', 'Test2')
+Sync-Unicorn -ControlPanelUrl $url -SharedSecret $secret -Configurations $configs
 
 # Note: you may pass -Verb 'Reserialize' for remote reserialize. Usually not needed though.
