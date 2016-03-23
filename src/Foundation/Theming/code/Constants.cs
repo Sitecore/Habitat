ed835b0c6a7c0723baf31fc82eb568decce8a88f
Thomas Eldblom <the@sitecore.net>
2016-03-23 11:45:10 +1100

    Added background styles to Theming project Added backgrounds the teasers Cleanup of the rendering parameters for sections and containers Added extension classes for rendering parameters

---
+++ src/Foundation/Theming/code/Constants.cs
@@ -0,0 +1,15 @@
+﻿namespace Sitecore.Foundation.Theming
+{
+  public struct Constants
+  {
+    public struct HasContainerLayoutParameters
+    {
+      public static string IsFluid => "ContainerIsFluid";
+    }
+
+    public struct BackgroundLayoutParameters
+    {
+      public static string Background => "Background";
+    }
+  }
+}
---
+++ src/Foundation/Theming/code/Extensions/RenderingExtensions.cs
@@ -0,0 +1,27 @@
+﻿namespace Sitecore.Foundation.Theming.Extensions
+{
+  using System;
+  using Sitecore.Data;
+  using Sitecore.Mvc.Presentation;
+
+  public static class RenderingExtensions
+  {
+    public static string GetBackgroundClass([NotNull] this Rendering rendering)
+    {
+      var id = MainUtil.GetID(rendering.Parameters[Constants.BackgroundLayoutParameters.Background] ?? "", null);
+      if (ID.IsNullOrEmpty(id))
+        return "";
+      var item = rendering.RenderingItem.Database.GetItem(id);
+      return item?[Templates.Style.Fields.Class] ?? "";
+    }
+
+    public static string GetContainerClass([NotNull] this Rendering rendering)
+    {
+      return rendering.IsContainerFluid() ? "container-fluid" : "container";
+    }
+    public static bool IsContainerFluid([NotNull] this Rendering rendering)
+    {
+      return MainUtil.GetBool(rendering.Parameters[Constants.HasContainerLayoutParameters.IsFluid], false);
+    }
+  }
+}
