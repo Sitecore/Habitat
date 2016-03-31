6527d335ff299c5279fdfc5a93e1131103da167d
OlegJytnik <olj@sitecore.net>
2016-03-31 12:24:45 +0300

    PasswordPreservWrapper provider has been added to the accounts project.

---
+++ src/Foundation/Accounts/code/Providers/PasswordPreservWrapper.cs
@@ -0,0 +1,20 @@
+﻿namespace Foundation.Accounts.Providers
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Web;
+  using System.Web.Security;
+
+  public class PasswordPreservWrapper : System.Web.Security.SqlMembershipProvider
+  {
+    public override MembershipUser CreateUser(string username, string password, string email, string passwordQuestion, string passwordAnswer, bool isApproved, object providerUserKey, out MembershipCreateStatus status)
+    {
+      var user = base.CreateUser(username, password, email, passwordQuestion, passwordAnswer, isApproved, providerUserKey, out status);
+      var sitecoreUser = Sitecore.Security.Accounts.User.FromName(username, true);
+      sitecoreUser.Profile["password"] = password;
+      sitecoreUser.Profile.Save();
+      return user;
+    }
+  }
+}
