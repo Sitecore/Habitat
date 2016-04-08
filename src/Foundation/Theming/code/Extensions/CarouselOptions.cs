f7c0501d445066c08c457be3dc913dc0de26fa0b
dashkodo <dashkodo@gmail.com>
2016-04-08 10:23:16 +0300

    Revert "Bug/design"

--- src/Foundation/Theming/code/Extensions/CarouselOptions.cs
+++
@@ -1,9 +0,0 @@
-namespace Sitecore.Foundation.Theming.Extensions
-{
-  public class CarouselOptions
-  {
-    public int ItemsShown { get; set; }
-    public bool AutoPlay { get; set; }
-    public bool ShowNavigation { get; set; }
-  }
-}
