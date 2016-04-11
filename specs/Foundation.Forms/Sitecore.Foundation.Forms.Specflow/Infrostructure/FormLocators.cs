0c7770472d4783a4f3ee2df12c921c44afc7de8f
Телтов Константин Алеексеевич <dneprokos@gmail.com>
2016-04-11 11:30:34 +0300

    Form Locators and BaseSteps were commited

---
+++ specs/Foundation.Forms/Sitecore.Foundation.Forms.Specflow/Infrostructure/FormLocators.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using Sitecore.Foundation.Common.Specflow.Extensions;
+using TechTalk.SpecFlow;
+
+namespace Sitecore.Foundation.Forms.Specflow.Infrostructure
+{
+  public class FormLocators
+  {
+    public static IWebDriver Driver => FeatureContext.Current.Get<IWebDriver>();
+
+    public IWebElement LeaveEmailFormField
+      => Driver.FindElement(By.CssSelector("#wffm3ac88418720c4072876e261d634c6e0e_Sections_0__Fields_0__Value"));
+
+    public IWebElement SubmitButton
+      => Driver.WaitUntilElementsPresent(By.CssSelector(".btn.btn-default")).First(el =>el.GetAttribute("type").Equals("submit"));
+
+  }
+}
---
+++ specs/Foundation.Forms/Sitecore.Foundation.Forms.Specflow/Steps/FormStepsBase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sitecore.Foundation.Common.Specflow.Extensions;
+using Sitecore.Foundation.Common.Specflow.Infrastructure;
+using Sitecore.Foundation.Common.Specflow.Steps;
+using Sitecore.Foundation.Forms.Specflow.Infrostructure;
+using TechTalk.SpecFlow;
+
+namespace Sitecore.Foundation.Forms.Specflow.Steps
+{
+  [Binding]
+  public class FormStepsBase : StepsBase
+  {
+    public NavigationStepsBase NavigationStepsBase => new NavigationStepsBase();
+
+    public CommonLocators BaseLocators => new CommonLocators();
+
+    public BaseSettings BaseSettings => new BaseSettings();
+
+    public CommonResults CommonResults => new CommonResults();
+
+    public FormLocators FormLocators = new FormLocators();
+
+    public xDBPanelResults XDbPanelResults = new xDBPanelResults();
+
+
+  }
+}
