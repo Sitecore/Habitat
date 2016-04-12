906868fbda0d4177c84c994d08219fb8791afed0
dmd <dmd@sitecore.net>
2016-04-12 11:51:29 +0300

    reverted Foundation.Theming

---
+++ src/Foundation/Theming/code/Constants.cs
@@ -0,0 +1,22 @@
+﻿namespace Sitecore.Foundation.Theming
+{
+  public struct Constants
+  {
+    public struct HasContainerLayoutParameters
+    {
+      public static string IsFluid => "ContainerIsFluid";
+    }
+
+    public struct BackgroundLayoutParameters
+    {
+      public static string Background => "Background";
+    }
+
+    public struct CarouselLayoutParameters
+    {
+      public static string ItemsShown => "ItemsShown";
+      public static string Autoplay => "Autoplay";
+      public static string ShowNavigation => "ShowNavigation";
+    }
+  }
+}
