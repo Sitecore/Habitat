f038ad8b8f39fbe406c021e21482105573713120
msv <msv@sitecore.net>
2016-04-13 17:04:54 +0300

    replace reporting db

---
+++ src/Foundation/Installer/code/ReportingDbReplace/DatabaseService.cs
@@ -0,0 +1,45 @@
+﻿namespace Sitecore.Foundation.Installer.ReportingDbReplace
+{
+  using System;
+  using System.Data.SqlClient;
+  using System.IO;
+  using Microsoft.SqlServer.Management.Common;
+  using Microsoft.SqlServer.Management.Smo;
+  using Sitecore.Diagnostics;
+
+  public class DatabaseService : IDatabaseService
+  {
+    public void ReplaceDatabase(string connectionString, string dbReplacementPath)
+    {
+      var connection = new SqlConnectionStringBuilder(connectionString);
+      ServerConnection serverConnection = null;
+      try
+      {
+        serverConnection = new ServerConnection(new SqlConnection(connectionString));
+        var server = new Server(serverConnection);
+
+        var database = server.Databases[connection.InitialCatalog];
+        var reportinPrimaryPath = database.PrimaryFilePath;
+        
+        server.KillAllProcesses(database.Name);
+        database.SetOffline();
+
+        File.Copy(dbReplacementPath, reportinPrimaryPath, true);
+        foreach (LogFile logFile in database.LogFiles)
+        {
+          logFile.Drop();
+        }
+
+        database.SetOnline();
+      }
+      catch (Exception ex)
+      {
+        Log.Error(ex.Message, ex, this);
+      }
+      finally
+      {
+        serverConnection?.Disconnect();
+      }
+    }
+  }
+}
