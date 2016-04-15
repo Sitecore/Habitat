d1f58fed4c8792423212c32363f919ef734b05f5
OlegJytnik <olj@sitecore.net>
2016-04-15 12:26:48 +0300

    Test project for Theming has been created.

---
+++ src/Foundation/Theming/tests/RenderingExtensionsTests.cs
@@ -0,0 +1,44 @@
+﻿namespace Sitecore.Foundation.Theming.Tests
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Text;
+  using System.Threading.Tasks;
+  using FluentAssertions;
+  using NSubstitute;
+  using Sitecore.Foundation.Theming.Extensions;
+  using Sitecore.Foundation.Theming.Tests.Extensions;
+  using Sitecore.Mvc.Presentation;
+  using Xunit;
+  using Ploeh.AutoFixture.AutoNSubstitute;
+  public class RenderingExtensionsTests
+  {
+    [Theory]
+    [AutoDbData]
+    public void IsFixedHeight_PropertySetInRendering_ShouldReturnTrue([Substitute]Rendering rendering)
+    {
+      var parameters = new RenderingParameters("Fixed height=1");
+      rendering.Parameters.Returns(parameters);
+      rendering.IsFixedHeight().Should().BeTrue();
+    }
+
+    [Theory]
+    [AutoDbData]
+    public void IsFixedHeight_PropertyIsNotSet_ShouldReturnTrue([Substitute]Rendering rendering)
+    {
+      var parameters = new RenderingParameters("");
+      rendering.Parameters.Returns(parameters);
+      rendering.IsFixedHeight().Should().BeFalse();
+    }
+
+    [Theory]
+    [AutoDbData]
+    public void IsFixedHeight_PropertySetToZero_ShouldReturnTrue([Substitute]Rendering rendering)
+    {
+      var parameters = new RenderingParameters("Fixed height=0");
+      rendering.Parameters.Returns(parameters);
+      rendering.IsFixedHeight().Should().BeFalse();
+    }
+  }
+}
