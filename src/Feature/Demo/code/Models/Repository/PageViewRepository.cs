02d019c04ddc32d67ee9f85005676d35ab8c5763
Thomas Eldblom <the@sitecore.net>
2016-04-28 14:53:38 +1000

    xDB Panel: Rearranged the page views list to avoid text overflow xDB Panel: Added full path tooltip to visits

--- src/Feature/Demo/code/Models/Repository/PageViewRepository.cs
+++ src/Feature/Demo/code/Models/Repository/PageViewRepository.cs
@@ -4,20 +4,20 @@
   using System.Linq;
   using Sitecore.Analytics.Core;
   using Sitecore.Analytics.Tracking;
-  using Sitecore.Data.Managers;
   using Sitecore.Foundation.SitecoreExtensions.Repositories;
 
   internal class PageViewRepository
   {
     public PageView Get(ICurrentPageContext pageContext)
     {
-      return new PageView()
+      return new PageView
              {
-               Duration = GetDuration(pageContext),
+               Duration = this.GetDuration(pageContext),
                HasEngagementValue = pageContext.PageEvents.Any(pe => pe.Value > 0),
                HasMvTest = HasMvTest(pageContext),
                HasPersonalisation = HasPersonalisation(pageContext),
-               Path = GetCleanPath(pageContext)
+               Path = this.GetCleanPath(pageContext),
+               FullPath = this.GetFullPath(pageContext)
       };
     }
 
@@ -36,18 +36,24 @@
       return TimeSpan.FromMilliseconds(pageContext.Duration);
     }
 
-    private string GetCleanPath(IPage page)
+    private string GetFullPath(IPage page)
     {
       var pageName = RemoveLanguage(page).Replace("//", "/").Remove(0, 1).Replace(".aspx", "");
-      if (pageName == string.Empty || IsLanguage(pageName))
+      if (pageName == string.Empty || this.IsLanguage(pageName))
       {
         pageName = DictionaryRepository.Get("/Demo/PageView/Home", "Home");
       }
+      return pageName;
+    }
+
+    private string GetCleanPath(IPage page)
+    {
+      var pageName = this.GetFullPath(page);
       if (pageName.Contains("/"))
       {
         pageName = "..." + pageName.Substring(pageName.LastIndexOf("/", StringComparison.Ordinal));
       }
-      return pageName.Length < 27 ? $"{pageName}" : $"{pageName.Substring(0, 26)}...";
+      return pageName.Length < 21 ? $"{pageName}" : $"{pageName.Substring(0, 20)}...";
     }
 
     private bool IsLanguage(string pageName)
