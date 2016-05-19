96080d8cf7cef5d15b216ab3e33237192aaf995f
Thomas Eldblom <the@sitecore.net>
2016-05-19 15:24:31 +1000

    Feature.Identity cleanup and code reformat

--- src/Feature/Identity/code/Repositories/IdentityRepository.cs
+++ src/Feature/Identity/code/Repositories/IdentityRepository.cs
@@ -1,13 +1,16 @@
 ﻿namespace Sitecore.Feature.Identity.Repositories
 {
-  using Sitecore;
+  using System;
   using Sitecore.Data.Items;
   using Sitecore.Foundation.SitecoreExtensions.Extensions;
 
   public static class IdentityRepository
   {
-    public static Item Get(Item contextItem)
+    public static Item Get([NotNull] Item contextItem)
     {
+      if (contextItem == null)
+        throw new ArgumentNullException(nameof(contextItem));
+
       return contextItem.GetAncestorOrSelfOfTemplate(Templates.Identity.ID) ?? Context.Site.GetContextItem(Templates.Identity.ID);
     }
   }
