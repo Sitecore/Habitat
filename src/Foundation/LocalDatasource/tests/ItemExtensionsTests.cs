295c912ba2484df9c960dc68b85b74545fda71c3
dmd <dmd@sitecore.net>
2016-06-07 10:44:28 +0300

    Added test project for LocalDatasource

---
+++ src/Foundation/LocalDatasource/tests/ItemExtensionsTests.cs
@@ -0,0 +1,28 @@
+﻿namespace Sitecore.Foundation.Datasource.Tests
+{
+  using FluentAssertions;
+  using Sitecore.Data.Items;
+  using Sitecore.FakeDb;
+  using Sitecore.FakeDb.AutoFixture;
+  using Sitecore.Foundation.LocalDatasource.Extensions;
+  using Sitecore.Foundation.Testing.Attributes;
+  using Xunit;
+
+  public class ItemExtensionsTests
+  {
+    [Theory]
+    [AutoDbData]
+    public void HasLocalDatasourceFolder_ItemWithoutLocalDatasource_ReturnsFalse([Content] Item item)
+    {
+      item.HasLocalDatasourceFolder().Should().BeFalse();
+    }
+
+    [Theory]
+    [AutoDbData]
+    public void HasLocalDatasourceFolder_ItemWithLocalDatasource_ReturnsFalse([Content] DbItem item, Db db)
+    {
+      item.Add(new DbItem("_Local"));
+      db.GetItem(item.ID).HasLocalDatasourceFolder().Should().BeTrue();
+    }
+  }
+}
