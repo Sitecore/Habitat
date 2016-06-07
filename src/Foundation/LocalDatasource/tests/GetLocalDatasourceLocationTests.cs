9a0a37bc635ddd0f7278fd3af520e948e0366cc6
dmd <dmd@sitecore.net>
2016-06-07 16:22:22 +0300

    Added GetLocalDatasourceLocation processor tests

---
+++ src/Foundation/LocalDatasource/tests/GetLocalDatasourceLocationTests.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sitecore.Foundation.LocalDatasource.Tests
+{
+  using FluentAssertions;
+  using Sitecore.Configuration;
+  using Sitecore.Data;
+  using Sitecore.Data.Items;
+  using Sitecore.FakeDb;
+  using Sitecore.FakeDb.AutoFixture;
+  using Sitecore.Foundation.LocalDatasource.Infrastructure.Pipelines;
+  using Sitecore.Foundation.Testing.Attributes;
+  using Sitecore.Pipelines.GetRenderingDatasource;
+  using Xunit;
+
+  public class GetLocalDatasourceLocationTests
+  {
+    [Theory]
+    [AutoDbData]
+    public void Process_LocalDatasourceExists_ShouldResolveDatasourceRoot(GetLocalDatasourceLocation processor,Db db, [Content]Item contextItem)
+    {
+      //arrange
+      db.Add(new DbItem("rendering") { { Templates.RenderingOptions.Fields.SupportsLocalDatasource, "1"}});
+
+      var datasourceFolder = contextItem.Add("_Local", contextItem.Template);
+
+      var renderingItem = db.GetItem("/sitecore/content/rendering");
+      var getRenderingDatasourceArgs = new GetRenderingDatasourceArgs(renderingItem)
+      {
+        ContextItemPath = contextItem.Paths.FullPath
+      };
+      //act
+      processor.Process(getRenderingDatasourceArgs);
+      //assert
+      getRenderingDatasourceArgs.DatasourceRoots.First().ID.Should().Be(datasourceFolder.ID);
+    }
+
+    [Theory]
+    [AutoDbData]
+    public void Process_LocalDatasourceNotExist_ShouldCreateDatasourceRoot(GetLocalDatasourceLocation processor, Db db, [Content]Item contextItem, [Content]DbTemplate template)
+    {
+      //arrange
+      db.Add(new DbItem("rendering") { { Templates.RenderingOptions.Fields.SupportsLocalDatasource, "1" } });
+
+      var renderingItem = db.GetItem("/sitecore/content/rendering");
+      var getRenderingDatasourceArgs = new GetRenderingDatasourceArgs(renderingItem)
+      {
+        ContextItemPath = contextItem.Paths.FullPath
+      };
+
+      //act
+      using (new SettingsSwitcher("Foundation.LocalDatasource.LocalDatasourceFolderTemplate", template.ID.ToString()))
+        processor.Process(getRenderingDatasourceArgs);
+      //assert
+      var datasourceFolder = contextItem.GetChildren().First();
+      getRenderingDatasourceArgs.DatasourceRoots.First().ID.Should().Be(datasourceFolder.ID);
+      datasourceFolder.TemplateID.Should().Be(template.ID);
+    }
+
+
+    [Theory]
+    [AutoDbData]
+    public void Process_ContextItemNotSet_ShouldReturnEmptyRoots(GetLocalDatasourceLocation processor, Db db, [Content]Item contextItem, [Content]DbTemplate template)
+    {
+      //arrange
+      db.Add(new DbItem("rendering") { { Templates.RenderingOptions.Fields.SupportsLocalDatasource, "1" } });
+
+      var renderingItem = db.GetItem("/sitecore/content/rendering");
+      var getRenderingDatasourceArgs = new GetRenderingDatasourceArgs(renderingItem)
+      {
+        ContextItemPath = ID.NewID.ToString()
+      };
+
+      //act
+      processor.Process(getRenderingDatasourceArgs);
+      //assert
+      getRenderingDatasourceArgs.DatasourceRoots.Should().BeEmpty();
+    }
+
+
+    [Theory]
+    [AutoDbData]
+    public void Process_SupportsLocalDatasourceFieldNotSet_ShouldReturnEmptyRoots(GetLocalDatasourceLocation processor, Db db, [Content]Item renderingItem)
+    {
+      var getRenderingDatasourceArgs = new GetRenderingDatasourceArgs(renderingItem);
+
+      //act
+      processor.Process(getRenderingDatasourceArgs);
+      //assert
+      getRenderingDatasourceArgs.DatasourceRoots.Should().BeEmpty();
+    }
+
+
+    [Theory]
+    [AutoDbData]
+    public void Process_DatasourceTemplateNotSet_ShouldReturnEmptyRoots(GetLocalDatasourceLocation processor, Db db, [Content]Item contextItem)
+    {
+      //arrange
+      db.Add(new DbItem("rendering") { { Templates.RenderingOptions.Fields.SupportsLocalDatasource, "1" } });
+
+      var renderingItem = db.GetItem("/sitecore/content/rendering");
+      var getRenderingDatasourceArgs = new GetRenderingDatasourceArgs(renderingItem)
+      {
+        ContextItemPath = contextItem.Paths.FullPath
+      };
+
+      //act
+      using (new SettingsSwitcher("Foundation.LocalDatasource.LocalDatasourceFolderTemplate", ID.NewID.ToString()))
+        processor.Process(getRenderingDatasourceArgs);
+      //assert
+      getRenderingDatasourceArgs.DatasourceRoots.Should().BeEmpty();
+    }
+
+  }
+}
--- src/Foundation/LocalDatasource/tests/UpdateLocalDatasourceReferencesService.cs
+++ src/Foundation/LocalDatasource/tests/UpdateLocalDatasourceReferencesService.cs
@@ -56,44 +56,6 @@
       }
     }
 
-
-    [Theory]
-    [AutoDbData]
-    public void UpdateAsync_ItemPassed_ShouldReplaceLinks([Substitute] LinkDatabase linkDb, Db db)
-    {
-      var datasourceItemId = ID.NewID;
-
-      db.Add(new DbItem("source") { Children = { new DbItem("_Local") { new DbItem("DatasourceItem") } }, Fields = { "testField" } }); ;
-      db.Add(new DbItem("target") { Children = { new DbItem("_Local") { new DbItem("DatasourceItem") } }, Fields = { "testField" } }); ;
-
-
-
-
-      var sourceItem = db.GetItem("/sitecore/content/source");
-      var targetItem = db.GetItem("/sitecore/content/target");
-      var datasourceItem = db.GetItem("/sitecore/content/source/_Local/DatasourceItem");
-      var targetDatasourceItem = db.GetItem("/sitecore/content/target/_Local/DatasourceItem");
-      var itemLinks = new[]
-      {
-        new ItemLink(sourceItem, FieldIDs.LayoutField, datasourceItem, string.Empty)
-      };
-
-      linkDb.GetReferences(sourceItem).Returns(itemLinks.ToArray());
-      using (new Sitecore.FakeDb.Links.LinkDatabaseSwitcher(linkDb))
-      {
-        using (new EditContext(targetItem))
-        {
-          targetItem["__Renderings"] = datasourceItem.ID.ToString();
-        }
-        var referenceReplacer = new UpdateLocalDatasourceReferencesService(sourceItem, targetItem);
-
-        referenceReplacer.UpdateAsync();
-        Jobs.JobManager.GetJob($"Resolving item references between source {AuditFormatter.FormatItem(sourceItem)} and target {AuditFormatter.FormatItem(targetItem)}.").Wait(int.MaxValue);
-
-        var expectedValue = targetDatasourceItem.ID.ToString();
-        targetItem["__Renderings"].Should().Be(expectedValue);
-      }
-    }
-
+    
   }
 }
