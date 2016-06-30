6143464b0502c77b4757720698f39fb258face52
dmd <dmd@sitecore.net>
2016-06-30 12:07:36 +0300

    Modified task to copy Sitecore license from installed instance

--- gulp-config.js
+++ gulp-config.js
@@ -1,7 +1,9 @@
 module.exports = function () {
+  var instanceRoot = "C:\\websites\\Habitat.dev.local";
   var config = {
-    websiteRoot: "C:\\websites\\Habitat.dev.local\\Website",
-    sitecoreLibraries: "C:\\websites\\Habitat.dev.local\\Website\\bin",
+    websiteRoot: instanceRoot + "\\Website",
+    sitecoreLibraries: instanceRoot + "\\Website\\bin",
+    licensePath: instanceRoot + "\\Data\\license.xml",
     solutionName: "Habitat",
     buildConfiguration: "Debug",
     runCleanBuilds: false
