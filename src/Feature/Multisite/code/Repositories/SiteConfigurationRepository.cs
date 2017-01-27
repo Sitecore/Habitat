8056f2a37b0d405f8de7fe5711b9031f11971735
Thomas Eldblom <the@sitecore.net>
2017-01-27 13:07:39 +1100

    Moved Danish language to Project.Habitat Changed multisite Site/ShowInMenu to shared

--- src/Feature/Multisite/code/Repositories/SiteConfigurationRepository.cs
+++ src/Feature/Multisite/code/Repositories/SiteConfigurationRepository.cs
@@ -1,66 +1,86 @@
 ﻿namespace Sitecore.Feature.Multisite.Repositories
 {
-  using System.Collections.Generic;
-  using System.Linq;
-  using Sitecore.Data.Items;
-  using Sitecore.Feature.Multisite.Models;
-  using Sitecore.Foundation.Multisite;
-  using Sitecore.Foundation.Multisite.Providers;
-  using Sitecore.Foundation.SitecoreExtensions.Extensions;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Sitecore.Configuration;
+    using Sitecore.Data.Items;
+    using Sitecore.Feature.Multisite.Models;
+    using Sitecore.Foundation.Multisite;
+    using Sitecore.Foundation.Multisite.Providers;
+    using Sitecore.Foundation.SitecoreExtensions.Extensions;
+    using Sitecore.Globalization;
 
-  public class SiteConfigurationRepository : ISiteConfigurationRepository
-  {
-    private readonly ISiteDefinitionsProvider siteDefinitionsProvider;
-
-    public SiteConfigurationRepository() : this(new SiteDefinitionsProvider())
+    public class SiteConfigurationRepository : ISiteConfigurationRepository
     {
-    }
+        private readonly ISiteDefinitionsProvider siteDefinitionsProvider;
 
-    public SiteConfigurationRepository(ISiteDefinitionsProvider itemSiteDefinitionsProvider)
-    {
-      this.siteDefinitionsProvider = itemSiteDefinitionsProvider;
-    }
+        public SiteConfigurationRepository() : this(new SiteDefinitionsProvider())
+        {
+        }
 
-    public SiteConfigurations Get()
-    {
-      var siteDefinitions = this.siteDefinitionsProvider.SiteDefinitions;
-      return this.Create(siteDefinitions);
-    }
+        public SiteConfigurationRepository(ISiteDefinitionsProvider itemSiteDefinitionsProvider)
+        {
+            this.siteDefinitionsProvider = itemSiteDefinitionsProvider;
+        }
 
-    private bool IsValidSiteConfiguration(SiteDefinition siteDefinition)
-    {
-      return siteDefinition.Item != null && this.IsSiteConfigurationItem(siteDefinition.Item);
-    }
+        public SiteConfigurations Get()
+        {
+            var siteDefinitions = this.siteDefinitionsProvider.SiteDefinitions;
+            return this.Create(siteDefinitions);
+        }
 
-    private bool IsSiteConfigurationItem(Item item)
-    {
-      return item.IsDerived(Multisite.Templates.SiteConfiguration.ID);
-    }
+        private bool IsValidSiteConfiguration(SiteDefinition siteDefinition)
+        {
+            return siteDefinition.Item != null && this.IsSiteConfigurationItem(siteDefinition.Item);
+        }
 
-    private SiteConfigurations Create(IEnumerable<SiteDefinition> definitions)
-    {
-      var siteDefinitions = new SiteConfigurations
-                            {
-                              Items = definitions.Where(this.IsValidSiteConfiguration).Select(CreateSiteConfiguration).Where(sc => sc.ShowInMenu)
-                            };
-      return siteDefinitions;
-    }
+        private bool IsSiteConfigurationItem(Item item)
+        {
+            return item.IsDerived(Multisite.Templates.SiteConfiguration.ID);
+        }
 
-    private static SiteConfiguration CreateSiteConfiguration(SiteDefinition siteConfiguration)
-    {
-      var title = siteConfiguration.Item[Multisite.Templates.SiteConfiguration.Fields.Title];
-      if (string.IsNullOrEmpty(title))
-      {
-        title = siteConfiguration.Name;
-      }
-      return new SiteConfiguration
-             {
-               HostName = siteConfiguration.HostName,
-               Name = siteConfiguration.Name,
-               Title = title,
-               ShowInMenu = siteConfiguration.Item.Fields[Multisite.Templates.SiteConfiguration.Fields.ShowInMenu].IsChecked(),
-               IsCurrent = siteConfiguration.IsCurrent
-             };
+        private SiteConfigurations Create(IEnumerable<SiteDefinition> definitions)
+        {
+            var siteDefinitions = new SiteConfigurations
+            {
+                Items = definitions.Where(this.IsValidSiteConfiguration).Select(CreateSiteConfiguration).Where(sc => sc.ShowInMenu)
+            };
+            return siteDefinitions;
+        }
+
+        private static SiteConfiguration CreateSiteConfiguration(SiteDefinition siteConfiguration)
+        {
+            return new SiteConfiguration
+            {
+                HostName = siteConfiguration.HostName,
+                Name = siteConfiguration.Name,
+                Title = GetSiteTitle(siteConfiguration),
+                ShowInMenu = siteConfiguration.Item.Fields[Multisite.Templates.SiteConfiguration.Fields.ShowInMenu].IsChecked(),
+                IsCurrent = siteConfiguration.IsCurrent
+            };
+        }
+
+        private static Item GetSiteItemInSiteLanguage(SiteDefinition siteConfiguration)
+        {
+            var siteItem = siteConfiguration.Item;
+            var siteLanguage = string.IsNullOrEmpty(siteConfiguration.Site.Language) ? Settings.DefaultLanguage : siteConfiguration.Site.Language;
+            using (new LanguageSwitcher(siteLanguage))
+            {
+                var item = siteConfiguration.Item.Database.GetItem(siteConfiguration.Item.ID);
+                if (item.HasContextLanguage())
+                    siteItem = item;
+            }
+            return siteItem;
+        }
+
+        private static string GetSiteTitle(SiteDefinition siteConfiguration)
+        {
+            var siteItem = GetSiteItemInSiteLanguage(siteConfiguration);
+
+            var title = (siteItem ?? siteConfiguration.Item)[Multisite.Templates.SiteConfiguration.Fields.Title];
+            if (string.IsNullOrEmpty(title))
+                title = siteConfiguration.Name;
+            return title;
+        }
     }
-  }
 }
