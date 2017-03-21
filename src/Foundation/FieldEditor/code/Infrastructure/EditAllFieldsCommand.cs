b69ea8a452bbe84d0602891a06470afb7bd2e5d9
Thomas Eldblom <the@sitecore.net>
2017-03-22 09:42:46 +1100

    Merge error

--- src/Foundation/FieldEditor/code/Infrastructure/EditAllFieldsCommand.cs
+++ src/Foundation/FieldEditor/code/Infrastructure/EditAllFieldsCommand.cs
@@ -1,68 +1,64 @@
 ﻿namespace Sitecore.Foundation.FieldEditor.Infrastructure
 {
-  using System.Collections.Specialized;
-  using System.Diagnostics.CodeAnalysis;
-  using System.Linq;
-  using Sitecore.Data;
-  using Sitecore.Data.Items;
-  using Sitecore.Diagnostics;
-  using Sitecore.Foundation.FieldEditor.Services;
-  using Sitecore.Shell.Applications.WebEdit;
-  using Sitecore.Shell.Applications.WebEdit.Commands;
-  using Sitecore.Shell.Framework.Commands;
-  using Sitecore.Web.UI.Sheer;
+    using System.Collections.Specialized;
+    using System.Diagnostics.CodeAnalysis;
+    using Sitecore.Data;
+    using Sitecore.Diagnostics;
+    using Sitecore.Foundation.FieldEditor.Services;
+    using Sitecore.Shell.Applications.WebEdit;
+    using Sitecore.Shell.Applications.WebEdit.Commands;
+    using Sitecore.Shell.Framework.Commands;
+    using Sitecore.Web.UI.Sheer;
 
-  public class EditAllFieldsCommand : FieldEditorCommand
-  {
-    private const string Parameter_Uri = "uri";
-    private const string Parameter_Fields = "fields";
-
-    [ExcludeFromCodeCoverage]
-    public override void Execute(CommandContext context)
+    public class EditAllFieldsCommand : FieldEditorCommand
     {
-      Assert.ArgumentNotNull(context, nameof(context));
-      if (context.Items.Length < 1)
-        return;
-      var args = new ClientPipelineArgs(context.Parameters);
-      var item = context.Items[0];
-      args.Parameters.Add(Parameter_Uri, item.Uri.ToString());
+        private const string Parameter_Uri = "uri";
+        private const string Parameter_Fields = "fields";
 
-      var pipedFieldNames = GetFieldsToEditService.GetFieldsToEdit(item);
+        [ExcludeFromCodeCoverage]
+        public override void Execute(CommandContext context)
+        {
+            Assert.ArgumentNotNull(context, nameof(context));
+            if (context.Items.Length < 1)
+            {
+                return;
+            }
+            var args = new ClientPipelineArgs(context.Parameters);
+            var item = context.Items[0];
+            args.Parameters.Add(Parameter_Uri, item.Uri.ToString());
 
-      args.Parameters[Parameter_Fields] = pipedFieldNames;
-      Context.ClientPage.Start(this, "StartFieldEditor", args);
-    }
+            var pipedFieldNames = GetFieldsToEditService.GetFieldsToEdit(item);
 
-    [ExcludeFromCodeCoverage]
-    protected override PageEditFieldEditorOptions GetOptions(ClientPipelineArgs args, NameValueCollection form)
-    {
-      Assert.ArgumentNotNull(args, nameof(args));
-      Assert.ArgumentNotNull(form, nameof(form));
-      Assert.IsNotNullOrEmpty(args.Parameters[Parameter_Uri], $"Field Editor command expects '{Parameter_Uri}' parameter");
-      Assert.IsNotNullOrEmpty(args.Parameters[Parameter_Fields], $"Field Editor command expects '{Parameter_Fields}' parameter");
+            args.Parameters[Parameter_Fields] = pipedFieldNames;
+            Context.ClientPage.Start(this, "StartFieldEditor", args);
+        }
 
-      var uri = ItemUri.Parse(args.Parameters[Parameter_Uri]);
-      Assert.IsNotNull(uri, Parameter_Uri + " parameter must be a valid Uri");
+        [ExcludeFromCodeCoverage]
+        protected override PageEditFieldEditorOptions GetOptions(ClientPipelineArgs args, NameValueCollection form)
+        {
+            Assert.ArgumentNotNull(args, nameof(args));
+            Assert.ArgumentNotNull(form, nameof(form));
+            Assert.IsNotNullOrEmpty(args.Parameters[Parameter_Uri], $"Field Editor command expects '{Parameter_Uri}' parameter");
+            Assert.IsNotNullOrEmpty(args.Parameters[Parameter_Fields], $"Field Editor command expects '{Parameter_Fields}' parameter");
 
-      var pipedFields = args.Parameters[Parameter_Fields];
+            var uri = ItemUri.Parse(args.Parameters[Parameter_Uri]);
+            Assert.IsNotNull(uri, Parameter_Uri + " parameter must be a valid Uri");
 
-      var item = Database.GetItem(uri);
-      Assert.IsNotNull(item, "item");
+            var pipedFields = args.Parameters[Parameter_Fields];
 
-      var options = GetFieldsToEditService.GetFieldEditorOptions(form, pipedFields, item);
+            var item = Database.GetItem(uri);
+            Assert.IsNotNull(item, "item");
 
-      return options;
-    }
+            var options = GetFieldsToEditService.GetFieldEditorOptions(form, pipedFields, item);
 
             return options;
         }
 
         public override CommandState QueryState(CommandContext context)
         {
-            return Context.PageMode.IsExperienceEditor
-                      && context.Items.Length >= 1 &&
-                      !string.IsNullOrEmpty(GetFieldsToEditService.GetFieldsToEdit(context.Items[0]))
-                      ? CommandState.Enabled : CommandState.Hidden;
+            if (!Context.PageMode.IsExperienceEditor)
+                return CommandState.Hidden;
+            return context.Items.Length >= 1 && !string.IsNullOrEmpty(GetFieldsToEditService.GetFieldsToEdit(context.Items[0])) ? CommandState.Enabled : CommandState.Hidden;
         }
     }
 }
