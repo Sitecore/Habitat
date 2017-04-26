dabc1e2d87419363aab794176a16d2e0f07c25a0
Thomas Eldblom <the@sitecore.net>
2017-04-26 13:53:18 +1000

    Fixed unit tests

--- src/Feature/News/Tests/NewsRepositoryTests.cs
+++ src/Feature/News/Tests/NewsRepositoryTests.cs
@@ -21,22 +21,6 @@
     {
         [Theory]
         [AutoDbData]
-        public void Get_ReturnsListOfNews([Frozen] ISearchServiceRepository searchServiceRepository, [Frozen] ISearchSettings searchSettings, string itemName, [Substitute] SearchService searchService)
-        {
-            var id = ID.NewID;
-            searchServiceRepository.Get(searchSettings).Returns(searchService);
-            var db = new Db
-            {
-                new DbItem(itemName, id, Templates.NewsFolder.ID)
-            };
-            var contextItem = db.GetItem(id);
-            var repository = new NewsRepository(searchServiceRepository);
-            var news = repository.Get(contextItem);
-            news.Should().As<IEnumerable<Item>>();
-        }
-
-        [Theory]
-        [AutoDbData]
         public void Get_NullAs1Parameter_ThrowArgumentNullException([Frozen] ISearchServiceRepository searchServiceRepository, [Frozen] ISearchSettings searchSettings)
         {
             var repo = new NewsRepository(searchServiceRepository);
