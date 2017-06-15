402e290440cb094e3d074794bf6b41534b95669a
Thomas Eldblom <the@sitecore.net>
2017-06-15 11:05:56 +1000

    Upgraded to latest theme Support for .user file in publishingsettings.target

--- src/Foundation/Theming/code/scripts/Sitecore.Foundation.Theming.js
+++ src/Foundation/Theming/code/scripts/Sitecore.Foundation.Theming.js
@@ -4260,7 +4260,7 @@ return EvEmitter;
 }));
 
 /*!
- * imagesLoaded v4.1.1
+ * imagesLoaded v4.1.2
  * JavaScript is all like "You images are done yet or what?"
  * MIT License
  */
@@ -4291,7 +4291,7 @@ return EvEmitter;
     );
   }
 
-})( window,
+})( typeof window !== 'undefined' ? window : this,
 
 // --------------------------  factory -------------------------- //
 
