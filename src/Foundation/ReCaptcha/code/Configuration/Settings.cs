9f6772a0d6841ca8496d0f94e07d3f7a105bd0b5
bii <isabilal@gmail.com>
2017-10-22 00:11:37 +0200

    Feature/ReCaptcha: support for ReCaptcha V2 and ReCaptcha invisible

--- src/Foundation/ReCaptcha/code/Configuration/Settings.cs
+++ src/Foundation/ReCaptcha/code/Configuration/Settings.cs
@@ -4,14 +4,14 @@
     {
         public struct V2
         {
-            public static string SiteKey => Sitecore.Configuration.Settings.GetSetting("Foundation.ReCaptcha.Sitekey", "6LeYNx0TAAAAAM5o86DlvSMR4xSCyu5pz7O0jujG");
-            public static string Secret => Sitecore.Configuration.Settings.GetSetting("Foundation.ReCaptcha.Secret", "6LeYNx0TAAAAAFfiFo8MmGjHhm3qyr47hhY-hRSB");
+            public static string SiteKey => Sitecore.Configuration.Settings.GetSetting("Foundation.ReCaptcha.Sitekey");
+            public static string Secret => Sitecore.Configuration.Settings.GetSetting("Foundation.ReCaptcha.Secret");
         }
 
         public struct Invisible
         {
-            public static string SiteKey => Sitecore.Configuration.Settings.GetSetting("Foundation.Invisible.ReCaptcha.Sitekey", "6Len-DQUAAAAAIi1x4JVNBn9gd4Tb81Yr-twfjVv");
-            public static string Secret => Sitecore.Configuration.Settings.GetSetting("Foundation.Invisible.ReCaptcha.Secret", "6Len-DQUAAAAACTW_yPX7A5iwTppYMojUDxmNbc0");
+            public static string SiteKey => Sitecore.Configuration.Settings.GetSetting("Foundation.Invisible.ReCaptcha.Sitekey");
+            public static string Secret => Sitecore.Configuration.Settings.GetSetting("Foundation.Invisible.ReCaptcha.Secret");
         }
         
     }
