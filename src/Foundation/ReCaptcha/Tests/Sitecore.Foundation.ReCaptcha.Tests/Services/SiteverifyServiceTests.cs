0c55ba5c65b4b116e411e617f5ae8a45aef40d2d
bii <isabilal@gmail.com>
2017-10-27 23:52:14 +0200

    Feature/ReCaptcha added unit tests

---
+++ src/Foundation/ReCaptcha/Tests/Sitecore.Foundation.ReCaptcha.Tests/Services/SiteverifyServiceTests.cs
@@ -0,0 +1,52 @@
+﻿using System.Threading.Tasks;
+using Sitecore.Foundation.ReCaptcha.Models;
+using Sitecore.Foundation.ReCaptcha.Services;
+using NSubstitute;
+using Xunit;
+using FluentAssertions;
+
+namespace Sitecore.Foundation.ReCaptcha.Tests.Services
+{
+    public class SiteverifyServiceTests
+    {
+
+        [Fact]
+        public async void Error_Codes_Should_Be_Null()
+        {
+            var siteverifyService = Substitute.For<ISiteverifyService>();
+
+            var response = new ReCaptchaResponseModel
+            {
+                ErrorCodes = { },
+                Success = true
+            };
+
+            siteverifyService.SiteVerifyAsync("").Returns(Task.FromResult(response));
+
+            var result = await siteverifyService.SiteVerifyAsync("");
+
+            result.ErrorCodes.Should().BeNull();
+            result.Success.Should().BeTrue();
+        }
+
+        [Fact]
+        public async void Error_Codes_Should_Not_Be_Empty()
+        {
+            var siteverifyService = Substitute.For<ISiteverifyService>();
+
+            var response = new ReCaptchaResponseModel
+            {
+                ErrorCodes = new[] { "" },
+                Success = false
+            };
+
+            siteverifyService.SiteVerifyAsync("").Returns(Task.FromResult(response));
+
+            var result = await siteverifyService.SiteVerifyAsync("");
+
+            result.ErrorCodes.Should().NotBeEmpty();
+            result.Success.Should().BeFalse();
+        }
+
+    }
+}
