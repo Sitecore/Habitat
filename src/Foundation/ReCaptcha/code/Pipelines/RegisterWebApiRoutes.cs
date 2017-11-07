cb87686447ad354c33ee06a7ab751615514dff72
bii <isabilal@gmail.com>
2017-11-07 20:12:41 +0100

    Feature/ReCaptcha added asynchronous Web API controller, async all the way

---
+++ src/Foundation/ReCaptcha/code/Pipelines/RegisterWebApiRoutes.cs
@@ -0,0 +1,14 @@
+﻿namespace Sitecore.Foundation.ReCaptcha.Pipelines
+{
+    using System.Web.Http;
+    using System.Web.Routing;
+    using Sitecore.Pipelines;
+
+    public class RegisterWebApiRoutes
+    {
+        public void Process(PipelineArgs args)
+        {
+            RouteTable.Routes.MapHttpRoute("Foundation.ReCaptcha.WebApi", "WebAPI/Foundation.ReCaptcha/{action}", new { controller = "ReCaptcha" });
+        }
+    }
+}
---
+++ src/Foundation/ReCaptcha/code/WebApi/ReCaptchaController.cs
@@ -0,0 +1,24 @@
+﻿namespace Sitecore.Foundation.ReCaptcha.WebApi
+{
+    using System.Threading.Tasks;
+    using System.Web.Http;
+    using Sitecore.Foundation.ReCaptcha.Models;
+    using Sitecore.Foundation.ReCaptcha.Services;
+
+    public class ReCaptchaController : ApiController
+    {
+        private readonly ISiteverifyService siteverifyService;
+
+        public ReCaptchaController()
+        {
+            this.siteverifyService = new SiteverifyService();
+        }
+
+        [HttpGet]
+        public async Task<ReCaptchaResponseModel> Siteverify(string response, bool invisible = false)
+        {
+            return await this.siteverifyService.SiteVerifyAsync(response, invisible);
+        }
+
+    }
+}
