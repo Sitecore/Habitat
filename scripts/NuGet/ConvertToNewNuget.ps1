a43981ce45bef89237bebf659200b8f2acadd142
Nick Wesselman <nwe@sitecore.net>
2019-02-14 10:06:23 -0500

    update nuget references for versioning changes

--- scripts/NuGet/ConvertToNewNuget.ps1
+++ scripts/NuGet/ConvertToNewNuget.ps1
@@ -1,21 +1,4 @@
-## Nuget v2 URL for the target platform, and the target marketing version
-$SitecoreNuget = "https://sitecore.myget.org/F/sc-platform-9-1/api/v2"
 $TargetVersion = "9.1.0"
-
-## Nuget URL to retrieve package metadata, including dependencies
-$MetaPackage = "$SitecoreNuget/Packages(Id='Sitecore.Experience.Platform',Version='$TargetVersion')"
-
-## Retrieve the package metadata from nuget (no nice way of doing this via CLI)
-$PackageMetadata = [xml](Invoke-WebRequest $MetaPackage).Content
-
-## Parse the package dependencies so we know the appropriate package versions for this release
-$versions = $PackageMetadata.entry.properties.Dependencies.Split('|') | % {
-    $dependencySplit = $_.Split(':')
-    New-Object PSObject -Property @{
-        Name = $dependencySplit[0]
-        Version = $dependencySplit[1]
-    }
-}
 
 ## Find all Visual Studio projects and parse their XML
 Get-ChildItem *.csproj -recurse | % {
@@ -35,13 +18,7 @@ Get-ChildItem *.csproj -recurse | % {
         if ($_.Include.EndsWith(".NoReferences")) {
             $_.SetAttribute("Include", $_.Include.Replace(".NoReferences", ""))
         }
-        ## Look up the appropriate version and replace
-        $packageName = $_.Include
-        $newPackage = $versions | ? { $_.Name -eq $packageName } | select -first 1
-        if (-not $newPackage) {
-            Write-Host "WARNING: Could not find new version for package $packageName in $fileName" -ForegroundColor yellow
-        }
-        $_.SetAttribute("Version", $newPackage.Version)
+        $_.SetAttribute("Version", $TargetVersion)
     }
 
     ## Save the project file
